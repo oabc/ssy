1947337d46dc34a6d84e268a18988179432c8a3a
yance <yance@email.com>
2017-01-12 00:22:18 +0800

    g

--- db_transfer.py
+++ db_transfer.py
@@ -21,60 +21,6 @@ class DbTransfer(object):
             DbTransfer.instance = DbTransfer()
         return DbTransfer.instance
 
-    def push_db_all_user(self):
-        #更新用户流量到数据库
-        last_transfer = self.last_get_transfer
-        curr_transfer = ServerPool.get_instance().get_servers_transfer()
-        #上次和本次的增量
-        dt_transfer = {}
-        for id in curr_transfer.keys():
-            if id in last_transfer:
-                if last_transfer[id][0] == curr_transfer[id][0] and last_transfer[id][1] == curr_transfer[id][1]:
-                    continue
-                elif curr_transfer[id][0] == 0 and curr_transfer[id][1] == 0:
-                    continue
-                elif last_transfer[id][0] <= curr_transfer[id][0] and \
-                last_transfer[id][1] <= curr_transfer[id][1]:
-                    dt_transfer[id] = [curr_transfer[id][0] - last_transfer[id][0],
-                                       curr_transfer[id][1] - last_transfer[id][1]]
-                else:
-                    dt_transfer[id] = [curr_transfer[id][0], curr_transfer[id][1]]
-            else:
-                if curr_transfer[id][0] == 0 and curr_transfer[id][1] == 0:
-                    continue
-                dt_transfer[id] = [curr_transfer[id][0], curr_transfer[id][1]]
-
-        self.last_get_transfer = curr_transfer
-        query_head = 'UPDATE user'
-        query_sub_when = ''
-        query_sub_when2 = ''
-        query_sub_in = None
-        last_time = time.time()
-        for id in dt_transfer.keys():
-            query_sub_when += ' WHEN %s THEN u+%s' % (id, dt_transfer[id][0])
-            query_sub_when2 += ' WHEN %s THEN d+%s' % (id, dt_transfer[id][1])
-            if query_sub_in is not None:
-                query_sub_in += ',%s' % id
-            else:
-                query_sub_in = '%s' % id
-        if query_sub_when == '':
-            return
-        query_sql = query_head + ' SET u = CASE port' + query_sub_when + \
-                    ' END, d = CASE port' + query_sub_when2 + \
-                    ' END, t = ' + str(int(last_time)) + \
-                    ' WHERE port IN (%s)' % query_sub_in
-        #print query_sql
-#UPDATE user SET u上传 = CASE port WHEN 10000 THEN u+79280 END, d下载 = CASE port WHEN 10000 THEN d+863188 END, t时间 = 1483353247 WHERE port IN (10000)
-        conn = cymysql.connect(host=Config.MYSQL_HOST, port=Config.MYSQL_PORT, user=Config.MYSQL_USER,
-                               passwd=Config.MYSQL_PASS, db=Config.MYSQL_DB, charset='utf8')
-        cur = conn.cursor()
-
-        cur.execute(query_sql)
-        cur.close()
-        conn.commit()
-        conn.close()
-
-
     @staticmethod
     def put_get_all(allflow):
         #数据库所有用户信息
@@ -124,8 +70,10 @@ class DbTransfer(object):
                 if curr_transfer[id][0] == 0 and curr_transfer[id][1] == 0:
                     continue
                 dt_transfer[id] = [curr_transfer[id][0], curr_transfer[id][1]]
-        self.last_get_transfer = curr_transfer
         #最后一次流量等于本次流量
+        if len(dt_transfer)<1 and len(curr_transfer)>0:
+            logging.info('return reason: not use flow')
+            return
         allflow = ''
         for id in dt_transfer.keys():
             allflow+='%s|%s|%s,' % (id, dt_transfer[id][0], dt_transfer[id][1])#(port,up,down)
@@ -144,7 +92,7 @@ class DbTransfer(object):
         #检查是否已经运行
         for row in rows:
             dt_alluser[row[0]]=row[0]
-            if ServerPool.get_instance().server_is_run(row[0]) is True:
+            if row[0] in dt_transfer.keys() m:
                 if ServerPool.get_instance().tcp_servers_pool[row[0]]._config['password'] != row[1]:
                     #password changed
                     logging.info('db restart server at port [%s] reason: password changed' % (row[0]))
@@ -156,25 +104,8 @@ class DbTransfer(object):
         #检查正在运行的
         for runport in dt_transfer.keys():
             if runport not in dt_alluser.keys():
-                logging.info('db restart server at port [%s] reason: password changed' % (runport))
+                logging.info('db stop server at port [%s] reason: run to stop' % (runport))
                 ServerPool.get_instance().del_server(runport)
-
-    @staticmethod
-    def del_server_out_of_bound_safe(rows):
-    #停止超流量的服务
-    #启动没超流量的服务
-    #修改下面的逻辑要小心包含跨线程访问
-        for row in rows:
-            if ServerPool.get_instance().server_is_run(row[0]) is True:
-                if ServerPool.get_instance().tcp_servers_pool[row[0]]._config['password'] != row[4]:
-                    #password changed
-                    logging.info('db restart server at port [%s] reason: password changed' % (row[0]))
-                    ServerPool.get_instance().del_server(row[0]) 
-                    ServerPool.get_instance().new_server(row[0], row[4])
-            else:
-                if row[5] == 1 and row[6] == 1 and row[1] + row[2] < row[3]:
-                    logging.info('db start server at port [%s] pass [%s]' % (row[0], row[4]))
-                    ServerPool.get_instance().new_server(row[0], row[4])
 
     @staticmethod
     def thread_db():
